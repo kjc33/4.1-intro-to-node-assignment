77ed048a41a073f14ed9daf607286dccae50dcfb
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-01-16 14:50:47 -0800

    Create Endpoints

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const http = require("http");
+const url = require("url");
 
 // Create a Server Object
-http.createServer(function (req, res) {
-    res.write("Hello Katie!");
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    if (req.url === "/profile") {
+      res.write("This is the /profile page!");
+    } else if (req.url === "/products") {
+      res.write("This is the /products page!");
+    } else if (req.url === "/cart") {
+      res.write("This is the /cart page!");
+    } else if (req.url === "/register") {
+      res.write("This is the /register page!");
+    } else if (req.url === "/login") {
+      res.write("This is the /login page!");
+    } else {
+      res.write(req.url);
+    }
     res.end();
-}).listen(8080);
+  })
+  .listen(8080);
