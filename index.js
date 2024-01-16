3b960cfceade16f34f3a90092b78b109e8109813
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-01-15 21:26:33 -0800

    Create Server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Huzzah!');
+const http = require("http");
+
+// Create a Server Object
+http.createServer(function (req, res) {
+    res.write("Hello Katie!");
+    res.end();
+}).listen(8080);
