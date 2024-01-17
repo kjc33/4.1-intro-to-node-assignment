4d742683b4cc870d4f6adf4c0892c524ee49f867
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-01-16 16:46:05 -0800

    Update Products Query

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const http = require("http");
 const url = require("url");
 
+let productsList = ["milk", "eggs", "cheese", "pork", "shrimp", "chicken"];
+
 // Create a Server Object
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    if (req.url === "/products") {
+    if (req.url === "/profile") {
+      res.write("This is the /profile page!");
+    } else if (req.url.startsWith("/products")) {
       let parsedUrl = url.parse(req.url, true);
       let search = parsedUrl.query;
       if (!search || !search.search) {
         res.write("This is the /products page!");
       } else {
-        let searchValue = search['search'] ? search['search'].toLowerCase() : null;
-        let productsList = ["milk", "eggs", "cheese", "pork", "shrimp", "chicken"];
+        let searchValue = search["search"] ? search["search"].toLowerCase() : null;
         let isProductFound = productsList.includes(searchValue);
         if (isProductFound === true) {
-          res.write(`Product ${search['search']} found.`);
+          res.write(`Product "${search["search"]}" found.`);
         } else {
-          res.write(`Product ${search['search']} not found.`);
+          res.write(`Product "${search["search"]}" not found.`);
         }
       }
+    } else if (req.url === "/profile") {
+      res.write("This is the /profile page!");
     } else if (req.url === "/cart") {
       res.write("This is the /cart page!");
     } else if (req.url === "/register") {
